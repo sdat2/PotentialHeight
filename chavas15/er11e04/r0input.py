0d5ad418bab0ea897f788549ea1aaf3a77da25db
Simon D.A. Thomas <sdat2@cam.ac.uk>
2023-12-04 18:34:14 +0000

    adding another bit of code

--- chavas15/er11e04/r0input.py
+++ chavas15/er11e04/r0input.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.interpolate import interp1d
 from chavas15.e04.outerwind_r0input_MM0 import E04_outerwind_r0input_nondim_MM0
 from chavas15.er11.radprof import ER11_radprof
+from chavas15.intersect import curveintersect
 
 
 def ER11E04_nondim_r0input(
@@ -32,6 +33,65 @@ def ER11E04_nondim_r0input(
     # Step 2: Converge rmaxr0 geometrically until ER11 M/M0 has tangent point with E04 M/M0
     # This step involves several iterations and calls to other functions like ER11_radprof
     # The detailed implementation of this step will depend on the logic and calculation specifics of the original MATLAB code
+
+    soln_converged = False
+    while not soln_converged:
+        # break up into 3 points, take 2 between which intersection vanishes, repat until this converges.
+        rmaxr0_min = 0.001
+        rmaxr0_max = 0.75
+        rmaxr0_new = (rmaxr0_max + rmaxr0_min) / 1  # guess middle
+        rmaxr0 = rmaxr0_new
+        drmaxr0 = rmaxr0_max - rmaxr0
+        drmaxr0_thresh = 1e-6
+        i = 0
+        rfracrm_min = 0  # [dimensionless] # start at r=0
+        rfracrm_max = 50  # [dimensionless] # many rmaxs away
+        while abs(drmaxr0) >= drmaxr0_thresh:
+            i += 1
+            rmax = rmaxr0_new * r0
+            drfacrm = 0.01
+            if rmax > 100 * 1000:  # large storm > 100km to rmax
+                drfracrm = drfracrm / 10  # extra precision for large storm
+            rrfracrm_ER11 = np.linspace(
+                rfracrm_min, rfracrm_max, num=(rfracrm_max - rfracrm_min) // drfacrm
+            )
+            rr_ER11 = rrfracrm_ER11 * rmax
+            rmax_or_r0 = "rmax"
+            VV_ER11, _ = ER11_radprof(Vmax, rmax, rmax_or_r0, fcor, CkCd, rr_ER11)
+
+            # what does this mean in matlab?
+            if np.isnan(max(VV_ER11)):  # ER11_radprof converged.
+                rrfracr0_ER11 = rr_ER11 / r0
+                MMfracM0_ER11 = (rr_ER11 * VV_ER11 + 0.5 * fcor * rr_ER11) / M0_E04
+
+                x0, y0 = curveintersect(
+                    rrfracr0_E04, MMfracM0_E04, rrfracr0_ER11, MMfracM0_ER11
+                )
+                if len(x0) == 0:
+                    drmaxr0 = abs(drmaxr0) / 2
+                else:
+                    drmaxr0 = -abs(drmaxr0) / 2
+                    rmerger0 = np.mean(x0)
+                    MmergeM0 = np.mean(y0)
+            else:
+                drmaxr0 = -abs(drmaxr0) / 2
+
+            rmaxr0 = rmaxr0_new
+            rmaxr0_new = rmaxr0_new + drmaxr0
+
+        if np.isnan(max(VV_ER11)) and rmerger0 is not None and var is not None:
+            soln_converged = True
+        else:
+            soln_converged = False
+            CkCd = CkCd + 0.1
+            print("Adjusting CkCd to find convergence.")
+    M0 = 0.5 * fcor * r0**2
+    Mm = 0.5 * fcor * rmax**2 + rmax * Vmax
+    MmM0 = Mm / M0
+    ii_ER11 = rrfracr0_ER11 < rmerger0 and MMfracM0_ER11 < MmergeM0
+    ii_E04 = rrfracr0_E04 >= rmerger0 and MMfracM0_E04 > MmergeM0
+    rrfracr0_temp = np.concat([rrfracr0_ER11(ii_ER11), rrfracr0_E04(ii_E04)])
+    MMfracM0_temp = np.concat([MMfracM0_ER11(ii_ER11), MMfracM0_E04(ii_E04)])
 
     # Final Step: Implement interpolation and calculation of final outputs
     # This will involve using the interp1d function from SciPy and the results from the previous steps
