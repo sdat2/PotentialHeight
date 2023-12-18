b86acb6fb9593881d164440ad80a1612af511b85
Simon D.A. Thomas <sdat2@cam.ac.uk>
2023-12-18 14:17:54 +0000

    running matlab from octave

--- cle/run.py
+++ cle/run.py
@@ -1,2 +1,11 @@
 import os
 import json
+from sithom.io import read_json
+
+print(read_json("inputs.json"))
+
+# run octave file r0_pm.m
+os.system("octave r0_pm.m")
+
+# read in the output from r0_pm.m
+print(read_json("outputs.json"))
