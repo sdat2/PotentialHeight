ceb8e06ccf4b7ab635d1d8200e4113f7ae6036fe
Simon D.A. Thomas <sdat2@cam.ac.uk>
2024-01-25 10:16:33 +0000

    adding a bit of docstring

--- tcpips/fort22.py
+++ tcpips/fort22.py
@@ -1,4 +1,11 @@
-"""Fort.22.nc file reader and writer."""
+"""Fort.22.nc file reader and writer.
+
+NWS 13 format:
+
+https://wiki.adcirc.org/NWS13
+
+
+"""
 from typing import Optional
 import os
 import xarray as xr
@@ -35,13 +42,20 @@ def trim_fort22():
     f22 = fort22.drop_nodes(["2004223N11301", "2004227N09314"])
     print(fort22.groups)
     print(f22.groups)
-    f22["2004217N13306"].name = "TC"
-    f22[""].attrs["group_order"] = ["Main", "TC"]
+    f22["2004217N13306"].name = "TC1"
+    f22[""].attrs["group_order"] = "Main TC1"
+    f22[""].attrs["institution"] = "Oceanweather Inc. (OWI)"
+    f22[""].attrs["conventions"] = "CF-1.6 OWI-NWS13"
+
     # f22.rename({"2004217N13306": "TC"})
     f22.to_netcdf(os.path.join(DATA_PATH, "test.nc"))
     ds = Dataset(os.path.join(DATA_PATH, "test.nc"))
     print(ds)
-    print(ds.groups["TC"])
+    print(ds.groups["TC1"])
+    print(ds.groups["Main"])
+
+    ds = Dataset(os.path.join(DATA_PATH, "fort.22.nc"))
+    print(ds)
     print(ds.groups["Main"])
 
 
