8302306fe27a543ab9b8aca32a8efd99af7a55da
Simon Thomas <sdat2@cam.ac.uk>
2024-10-20 22:36:31 +0100

    varya gamma beta

--- worst/vary_gamma_beta.py
+++ worst/vary_gamma_beta.py
@@ -1,3 +1,4 @@
+import os
 from typing import List
 import hydra
 import numpy as np
@@ -7,12 +8,12 @@ from omegaconf import DictConfig
 from .constants import CONFIG_PATH
 from .tens import (
     seed_all,
-    tfd,
     gen_data,
     fit_gev_upper_bound_not_known,
     fit_gev_upper_bound_known,
 )
 from .utils import alpha_from_z_star_beta_gamma
+from .constants import DATA_PATH
 
 # @timeit
 # def try_fit():
@@ -75,14 +76,14 @@ def try_fit(
     low_q = genextreme.isf(quantiles, c=-bg_gamma, loc=bg_alpha, scale=bg_beta)
     up_known_q = genextreme.isf(quantiles, c=-s_gamma, loc=s_alpha, scale=s_beta)
     results = np.array([true_q, low_q, up_known_q])
-    print(results.shape)
-    print(results)
-    print(np.expand_dims(np.expand_dims(results, 0), -1).shape)
     return xr.Dataset(
         data_vars={
             "rv": (
-                ("ns", "fit", "rp", "seed"),
-                np.expand_dims(np.expand_dims(results, 0), -1),
+                ("ns", "fit", "rp", "seed", "beta", "gamma"),
+                np.expand_dims(
+                    np.expand_dims(np.expand_dims(np.expand_dims(results, 0), -1), -1),
+                    -1,
+                ),
                 {"units": "m"},
             )
         },
@@ -91,15 +92,52 @@ def try_fit(
             "fit": (("fit"), ["true", "max_known", "max_unknown"]),
             "rp": (
                 ("rp"),
-                [100, 500],
+                [int(1 / q) for q in quantiles],
                 {"units": "years", "long_name": "Return period"},
             ),
             "seed": (
                 ("seed"),
                 [seed],
             ),
+            "gamma": (("gamma"), [gamma]),
+            "beta": (("beta"), [beta]),
         },
     )["rv"]
+
+
+def make_fit_da(config: DictConfig) -> xr.DataArray:
+    quantiles = list(config.quantiles)
+
+    betas = np.linspace(
+        config.beta_range.min, config.beta_range.max, config.beta_range.steps
+    )
+    gammas = np.linspace(
+        config.gamma_range.min, config.gamma_range.max, config.gamma_range.steps
+    )
+    seed_offsets = np.linspace(0, config.seed_steps_Nr, config.seed_steps_Nr)
+    da = xr.concat(
+        [
+            xr.concat(
+                [
+                    xr.concat(
+                        [
+                            try_fit(
+                                config.z_star, beta, gamma, config.ns, seed, quantiles
+                            )
+                            for beta in betas
+                        ],
+                        dim="beta",
+                    )
+                    for gamma in gammas
+                ],
+                dim="gamma",
+            )
+            for seed in seed_offsets
+        ],
+        dim="seed",
+    )
+    da.to_netcdf(os.path.join(DATA_PATH, "vary_gamma_beta.nc"))
+    return da
 
 
 @hydra.main(version_base=None, config_path=CONFIG_PATH, config_name="vary_gamma_beta")
@@ -112,18 +150,7 @@ def run_vary_gamma_beta(config: DictConfig) -> None:
     Args:
         config (DictConfig): Hydra config object.
     """
-    print(config)
-    seed_all(config.seed)
-
-    da = try_fit(
-        config.z_star,
-        1,
-        -0.2,
-        50,
-        config.seed,
-        list(config.quantiles),
-    )
-    print(da)
+    make_fit_da(config)
 
 
 if __name__ == "__main__":
