fixed potential intensity calculation script: now includes time etc.

cle/potential_size.py
@@ -10,14 +10,12 @@ from sithom.io import read_json, write_json
 from sithom.plot import plot_defaults
 from .constants import (
     BACKGROUND_PRESSURE,
-    DEFAULT_SURF_TEMP,
     DATA_PATH,
     FIGURE_PATH,
     SRC_PATH,
     F_COR_DEFAULT,
     W_COOL_DEFAULT,
     RHO_AIR_DEFAULT,
-    SUPERGRADIENT_FACTOR,
     LATENT_HEAT_OF_VAPORIZATION,
     GAS_CONSTANT_FOR_WATER_VAPOR,
     GAS_CONSTANT,
@@ -42,7 +40,9 @@ plot_defaults()
 
 def _inputs_to_name(inputs: dict) -> str:
     """Create a unique naming string based on the input parameters
-    (could be hashed to shorten).
+    (now hashed to shorten).
+
+    There is a small probability of a hash collision during runs.
 
     Args:
         inputs (dict): input dict
@@ -83,9 +83,12 @@ def _run_cle15_octave(inputs: dict) -> dict:
 
     ins = process_inputs(inputs)
 
+    print("inputs", inputs)
+
     # Storm parameters
     name = _inputs_to_name(ins)
 
+    # write input file for octave to read
     write_json(ins, os.path.join(DATA_PATH, "tmp", name + "-inputs.json"))
 
     # run octave file r0_pm.m
@@ -113,9 +116,8 @@ def run_cle15(
     Returns:
         Tuple[float, float, float, float]: pm [Pa], rmax [m], vmax [m/s], pc [Pa]
     """
-
-    ou = _run_cle15_octave(inputs)
     ins = process_inputs(inputs)  # find old data.
+    ou = _run_cle15_octave(inputs)
 
     if plot:
         # print(ou)
@@ -154,10 +156,13 @@ def run_cle15(
 
     # integrate the wind profile to get the pressure profile
     # assume wind-pressure gradient balance
-    p0 = ins["p0"] * 100  # [Pa]
+    p0 = ins["p0"] * 100  # [Pa] [originally in hPa]
     rho0 = RHO_AIR_DEFAULT  # [kg m-3]
     rr = np.array(ou["rr"])  # [m]
     vv = np.array(ou["VV"])  # [m/s]
+    print("rr", rr[:10], rr[-10:])
+    print("vv", vv[:10], vv[-10:])
+    vv[-1] = 0
     p = pressure_from_wind(rr, vv, p0=p0, rho0=rho0, fcor=ins["fcor"])  # [Pa]
     ou["p"] = p.tolist()
 

cle/utils.py
@@ -3,7 +3,7 @@ Utilities for idealized tropical cyclone calculations.
 """
 
 import numpy as np
-from scipy.integrate import cumulative_simpson
+from scipy.integrate import cumulative_simpson, cumulative_trapezoid
 from .constants import TEMP_0K
 
 
@@ -29,7 +29,7 @@ def coriolis_parameter_from_lat(lat: np.ndarray) -> np.ndarray:
     return 2 * 7.2921e-5 * np.sin(np.deg2rad(lat))
 
 
-def pressure_from_wind_old(
+def pressure_from_wind(
     rr: np.ndarray,  # [m]
     vv: np.ndarray,  # [m/s]
     p0: float = 1015 * 100,  # [Pa]
@@ -76,7 +76,7 @@ def pressure_from_wind_old(
     return p  # pressure profile [Pa]
 
 
-def pressure_from_wind(
+def pressure_from_wind_new(
     rr: np.ndarray,  # [m]
     vv: np.ndarray,  # [m s-1]
     p0: float = 1015 * 100,  # [Pa]
@@ -100,18 +100,18 @@ def pressure_from_wind(
 
     and then bringing p(r) to the left hand side yields
 
-    \frac{dlog(p)}{dr} = \frac{rho0}{p0}\left(\frac{v(r)^2}{r} +fv(r)\right),
+    \frac{1}{p}\frac{dp}{dr}=\frac{dlog(p)}{dr} = \frac{rho0}{p0}\left(\frac{v(r)^2}{r} +fv(r)\right),
 
-    allowing us to express p(r) by integrating from infinity to r:
+    allowing us to express p(r) by integrating from infinity to r,
 
-    p(r) = p0*\exp\left(\frac{rho0}{p0}\int^{\inf}_{r}\left(\frac{v(r)^2}{r} +fv(r)\right)\right) dr
+    p(r) = p0*\exp\left(\frac{rho0}{p0}\int^{\inf}_{r}\left(\frac{v(r)^2}{r} +fv(r)\right)dr\right).
 
     Args:
         rr (np.ndarray): radii. Ascending order [m].
         vv (np.ndarray): velocities. From center to edge [m s-1].
-        p0 (float): background pressure. Defaults to 1015_00 [Pa].
-        rho0 (float): background density. Defaults to 1.15 [kg m-3].
-        fcor (float): coriolis force. Defaults to 5e-5 [s-1].
+        p0 (float, optional): background pressure. Defaults to 1015_00 [Pa].
+        rho0 (float, optional): background density. Defaults to 1.15 [kg m-3].
+        fcor (float, optional): coriolis force. Defaults to 5e-5 [s-1].
 
     Returns:
         np.ndarray: pressure profile [Pa].
@@ -131,6 +131,13 @@ def pressure_from_wind(
         >>> np.allclose(p, np.array([1015_00] * 6), rtol=1e-3, atol=1e-6)
         True
     """
+    # all real inputs
+    assert isinstance(rho0, float) and isinstance(p0, float) and isinstance(fcor, float)
+    assert rho0 < 2 and rho0 > 0.5  # definitely between 0.5 and 2 kg m-3
+    assert p0 > 900_00 and p0 < 1100_00  # definitely between 900 hPa and 1100 hPa
+    assert (
+        fcor > 0
+    )  # assuming cyclonic winds, just take absolute value if you are in southern hemisphere
     if isinstance(rr, list):
         rr = np.array(rr)
     if isinstance(vv, list):
@@ -140,8 +147,11 @@ def pressure_from_wind(
     integrand = (
         (vv**2 / (rr + 1e-6) + fcor * vv) * rho0 / p0
     )  # adding small delta 1e-6 to avoid singularity
-    integral = cumulative_simpson(integrand[::-1], rr[::-1], initial=0)
-    return p0 * np.exp(integral[::-1])
+    integral = cumulative_trapezoid(integrand[::-1], rr[::-1], initial=0)[::-1]
+    p = p0 * np.exp(integral)
+    print(p)
+    assert p[0] <= p[-1]
+    return p
 
 
 def buck_sat_vap_pressure(