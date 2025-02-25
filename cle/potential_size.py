fixed most errors in comparison test, changed some options for potential size calculation

cle/potential_size.py
@@ -166,6 +166,7 @@ def _run_cle15_octave(inputs: dict) -> dict:
 def run_cle15(
     plot: bool = False,
     inputs: Optional[Dict[str, any]] = None,
+    rho0=RHO_AIR_DEFAULT,  # [kg m-3]
 ) -> Tuple[float, float, float]:  # pm, rmax, vmax, pc
     """
     Run the CLE15 model.
@@ -218,7 +219,6 @@ def run_cle15(
     # integrate the wind profile to get the pressure profile
     # assume wind-pressure gradient balance
     p0 = ins["p0"] * 100  # [Pa] [originally in hPa]
-    rho0 = RHO_AIR_DEFAULT  # [kg m-3]
     ou["VV"][-1] = 0  # get rid of None at end of output.
     assert None not in ou["VV"]
     rr = np.array(ou["rr"], dtype="float32")  # [m]
@@ -240,8 +240,8 @@ def run_cle15(
 
     # plot the pressure profile
     return (
-        interp1d(rr, p)(
-            ou["rmax"]
+        float(
+            interp1d(rr, p)(ou["rmax"])
         ),  # find the pressure at the maximum wind speed radius [Pa]
         ou["rmax"],  # rmax radius [m]
         p[0],

cle/test.py
@@ -1,5 +1,5 @@
 """
-Test solutions against Wang 2022s figure and paper data
+Test solutions against Wang 2022 figure and paper data
 (make sure that everything works as expected)
 """
 
@@ -61,6 +61,8 @@ def test_figure_4():
     env_humidity = 0.9
     near_surface_air_temperature = 299  # K
 
+    vsg = 83 / 1.2
+
     water_vapour_pressure = env_humidity * buck_sat_vap_pressure(
         near_surface_air_temperature
     )
@@ -77,10 +79,12 @@ def test_figure_4():
                 "supergradient_factor": 1.2,  # dimensionless
                 "t0": 200,  # K
                 "w_cool": 0.002,  # m s-1
-                "vmax": 83,  # m s-1
+                "vmax": vsg,  # m s-1
                 "msl": 1015,  # mbar
-                # "rho_air": rho,  # kg m-3
+                "rho_air": rho_air,  # kg m-3
                 "env_humidity": 0.9,
+                "cd_ck": 1,
+                "cd": 0.0015,
             },
             coords={"lat": lat},
         ),
@@ -97,10 +101,10 @@ def test_figure_4():
         soln_ds.r0.values / 1000,
         soln_ds.pm.values / 100,
         marker="x",
-        label="bisection point solution",
+        label="Our bisection point solution",
     )
 
-    r0s = cle15_test.iloc[:, 0]
+    r0s = cle15_test.iloc[:, 0] * 1000  # convert to meters
     pm_cle15 = []
     pc_cle15 = []
     rmax_cle15 = []
@@ -109,57 +113,89 @@ def test_figure_4():
             plot=False,
             inputs={
                 "r0": r0,
-                "Vmax": 83,
+                "Vmax": vsg,
                 "w_cool": 0.002,
                 "fcor": 5e-5,
                 "p0": 1015,
+                "Cd": 0.0015,
+                "CkCd": 1,
             },
         )
-        pm_cle15.append(pm_cle)
-        pc_cle15.append(pc_cle)
-        rmax_cle15.append(rmax_cle)
+        pm_cle15.append(pm_cle / 100)
+        pc_cle15.append(pc_cle / 100)
+        rmax_cle15.append(rmax_cle / 1000)
+    pd.DataFrame(
+        {
+            "r0": r0s / 1000,
+            "pm_cle15": pm_cle15,
+            "pc_cle15": pc_cle15,
+        }
+    ).to_csv(os.path.join(DATA_PATH, "w22", "4a-cle15-ours.csv"), index=False)
 
-    plt.plot(r0s, np.array(pm_cle15) / 100, label="CLE15 $p_m$")
-    plt.plot(r0s, np.array(pc_cle15) / 100, label="CLE15 $p_c$")
+    print("pm_cle15 [mbar]", pm_cle15)
+    print("pc_cle15 [mbar]", pc_cle15)
+    print("rmax_cle15 [km]", rmax_cle15)
 
-    r0s = w22_test.iloc[:, 0]
+    plt.plot(r0s / 1000, pm_cle15, label="Our CLE15 $p_m$")
+    plt.plot(r0s / 1000, pc_cle15, label="Our CLE15 $p_c$")
+
+    r0s = w22_test.iloc[:, 0] * 1000  # convert to meters
     pm_w22 = []
 
     for r0 in r0s:
-        _, rmax_cle, _ = run_cle15(
-            plot=False,
+        pm, rmax_cle, pc = run_cle15(
             inputs={
                 "r0": r0,
-                "Vmax": 83,
+                "Vmax": vsg,
                 "w_cool": 0.002,
                 "fcor": 5e-5,
                 "p0": 1015,
+                "Cd": 0.0015,
+                "CkCd": 1,
             },
+            rho0=rho_air,
         )
+        print(
+            "r0", r0, "rmax", rmax_cle, "pm", pm / 100, "mbar", "pc", pc / 100, "mbar"
+        )
+        assert rmax_cle > 1000  # should be in meters
         yval = bisection(
             wang_diff(
                 *wang_consts(
                     radius_of_max_wind=rmax_cle,
                     radius_of_inflow=r0,
-                    maximum_wind_speed=83 * 1.2,
+                    maximum_wind_speed=vsg * 1.2,
                     coriolis_parameter=5e-5,
-                    pressure_dry_at_inflow=1015 - 0.9 * buck_sat_vap_pressure(299),
+                    pressure_dry_at_inflow=1015 * 100
+                    - 0.9 * buck_sat_vap_pressure(299),
                     near_surface_air_temperature=299,
                     outflow_temperature=200,
                 )
             ),
             0.5,
-            1.2,
+            1.5,
             1e-6,
         )
         # convert solution to pressure
         pm_w22_car = (
             1015 * 100 - buck_sat_vap_pressure(299)
         ) / yval + buck_sat_vap_pressure(299)
+        print("rmax", rmax_cle, "pm_w22_car", pm_w22_car / 100, "mbar")
         pm_w22.append(pm_w22_car / 100)
 
-    plt.plot(r0s, pm_w22, label="W22 $p_m$")
+    print("pm_w22", pm_w22)
+
+    plt.plot(r0s / 1000, pm_w22, label="Our W22 $p_m$")
+    # write a csv with the results
+    os.makedirs(os.path.join(DATA_PATH, "w22"), exist_ok=True)
+    pd.DataFrame(
+        {
+            "r0": r0s / 1000,
+            "pm_w22": pm_w22,
+        }
+    ).to_csv(os.path.join(DATA_PATH, "w22", "4a-w22-ours.csv"), index=False)
     plt.legend(ncol=2, loc="lower center", bbox_to_anchor=(0.5, 1.05))
+    plt.xlim(200, 5000)
     os.makedirs(os.path.join(FIGURE_PATH, "w22"), exist_ok=True)
     plt.savefig(os.path.join(FIGURE_PATH, "w22", "figure_4.pdf"))
 