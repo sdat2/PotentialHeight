10114c4bc5f886d7459ea6eabb8b62fdb27f7c1e
Simon D.A. Thomas <sdat2@cam.ac.uk>
2025-04-14 23:50:29 +0100

    plotted dual graph depth properly

--- adforce/dual_graph.py
+++ adforce/dual_graph.py
@@ -1,6 +1,7 @@
 """Dual graph plotting module."""
 
 import os
+import numpy as np
 from matplotlib import pyplot as plt
 from sithom.plot import plot_defaults, get_dim, label_subplots
 from sithom.time import timeit
@@ -27,25 +28,32 @@ def plot_dual_graph() -> None:
     plot_defaults()
     fig, axs = plt.subplots(3, 1, figsize=get_dim(ratio=1.1))
     im = axs[0].scatter(
-        dg.x.values, dg.y.values, c=dg.depth.values, s=0.1, cmap="viridis"
+        dg.x.values, dg.y.values, c=dg.depth.values, s=0.1, cmap="viridis_r"
     )
-    plt.colorbar(im, ax=axs[0], label="Depth [m]", orientation="vertical", shrink=0.3)
+    axs[0].set_title("Depth [m]")
+    plt.colorbar(im, ax=axs[0], orientation="vertical")  # , shrink=0.3
     # set equal aspect ratio
     axs[0].set_aspect("equal")
     NO_BBOX.ax_lim(axs[0])
+    vmin = dg.depth_grad.values.min()
+    vmax = dg.depth_grad.values.max()
+    vmin, vmax = np.min([-vmax, vmin]), np.max([-vmin, vmax])
+
     im = axs[1].scatter(
         dg.x.values,
         dg.y.values,
         c=dg.depth_grad.values[0, :],
         s=0.1,
-        cmap="viridis",
+        cmap="cmo.balance",
+        vmin=vmin,
+        vmax=vmax,
     )
+    axs[1].set_title("Depth $x$ Gradient [m $^{\circ}\;^{-1}$]")
     plt.colorbar(
         im,
         ax=axs[1],
-        label="Depth $x$ Gradient [m $^{\circ}\;^{-1}$]",
         orientation="vertical",
-        shrink=0.3,
+        # shrink=0.3,
     )
     axs[1].set_aspect("equal")
     NO_BBOX.ax_lim(axs[1])
@@ -54,16 +62,19 @@ def plot_dual_graph() -> None:
         dg.y.values,
         c=dg.depth_grad.values[1, :],
         s=0.1,
-        cmap="viridis",
+        cmap="cmo.balance",
+        vmin=vmin,
+        vmax=vmax,
     )
+    axs[2].set_title("Depth $y$ Gradient [m $^{\circ}\;^{-1}$]")
     plt.colorbar(
         im,
         ax=axs[2],
-        label="Depth $y$ Gradient [m $^{\circ}\;^{-1}$]",
         orientation="vertical",
-        shrink=0.3,
+        # shrink=0.3,
     )
     axs[2].set_aspect("equal")
+    NO_BBOX.ax_lim(axs[2])
     label_subplots(axs)
     axs[2].set_xlabel("Longitude [$^{\circ}$E]")
     axs[2].set_ylabel("Latitude [$^{\circ}$N]")
