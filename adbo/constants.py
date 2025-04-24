251045f24c5d239fea7c499a5a96bd511091ae12
Simon Thomas <sdat2@cam.ac.uk>
2025-04-24 16:47:38 +0100

    adbo data path

--- adbo/constants.py
+++ adbo/constants.py
@@ -21,6 +21,8 @@ SRC_PATH = pathlib.Path(os.path.dirname(constants_path))
 PROJECT_PATH = pathlib.Path(os.path.dirname(SRC_PATH))
 FIGURE_PATH = pathlib.Path(os.path.join(PROJECT_PATH, "img"))
 CONFIG_PATH = os.path.join(SRC_PATH, "config")
+DATA_PATH = os.path.join(PROJECT_PATH, "data", "adbo")
+os.makedirs(DATA_PATH, exist_ok=True)
 AD_PATH = pathlib.Path(
     os.path.dirname(PROJECT_PATH)
 )  # assume adcirc is in the parent directory of the project
